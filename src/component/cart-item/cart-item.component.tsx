c59305683ade09c8530f062a57cb93ce7161bee1
Nikhil Sinha <nikhilsinha198@gmail.com>
2022-08-08 16:21:42 +0530

    Day2 not exacitly but done most of the work of the store

---
+++ src/component/cart-item/cart-item.component.tsx
@@ -0,0 +1,37 @@
+import './cart-item.styles.scss';
+
+export type Date = {
+	id: number;
+	name: string;
+	imageUrl: string;
+	price: number;
+};
+
+export const CartItem = ({ id, name, imageUrl, price }: Date) => {
+	let quantity = 0;
+	return (
+		<div className='item-container' key={id}>
+			<img src={imageUrl} alt={`${name}`} />
+			<div className='name-container'>
+				<span className='name'>{name}</span>
+				<span className='price'>{price}</span>
+			</div>
+			<div className='addToCart'>
+				{quantity === 0 ? (
+					<button className='adCart'>+ add to cart</button>
+				) : (
+					<div className='btn_container'>
+						<div className='remove'>
+							<button>+</button>
+							<span>{quantity} </span> in the cart
+							<button>-</button>
+						</div>
+						<button>remove</button>
+					</div>
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default CartItem;
--- src/pages/Store/store.component.tsx
+++ src/pages/Store/store.component.tsx
@@ -1,5 +1,18 @@
+import shopData from '../../data/shopData.json';
+import CartItem from '../../component/cart-item/cart-item.component';
+import './store.styles.scss';
+
 const Store = () => {
-	return <h2>Store</h2>;
+	return (
+		<>
+			<h2 className='h2'>Store</h2>
+			<div className='data_container'>
+				{shopData.map((data) => (
+					<CartItem {...data} />
+				))}
+			</div>
+		</>
+	);
 };
 
 export default Store;
